5dc7cf206dfb359b4e85d27d92212c6d9ec91883
Jonh Zono <jonhzono@gmail.com>
2020-01-22 15:29:16 +0900

    Prepare for deployment

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv').config();
+const path = require('path');
 const PORT = 8080;
 
 // Setup express app
@@ -40,7 +41,14 @@ app.use('/api/post', require('./route/api/post'));
 app.use('/api/exam', require('./route/api/examScore'));
 app.use('/api/duefee', require('./route/api/dueFee'));
 
-app.get('/', (req, res) => {
-  res.send('SMS Backend Server');
-});
+//server static assets in production
+if (process.env.NODE_ENV === 'production') {
+  //set static folder
+  app.user(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.htm'));
+  });
+}
+
 app.listen(PORT, () => console.log(`Node JS is running on port ${PORT}`));
